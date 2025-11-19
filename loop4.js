b8167f085fc1bf04ede8a07d450e822c638a9faf
Vishnu Ojha <vishnuojha@Vishnus-Mac-mini.local>
2025-11-19 07:59:17 +0530

    for loop inside for loop

---
+++ loop4.js
@@ -0,0 +1,26 @@
+// loop within loop
+
+// i, j values are independent
+// for(let i=0; i<5; i++){
+//     for( let j=0; j<5; j++){
+//         console.log(i,j)
+//     }
+// }
+
+// for(let i=0; i<5; i++){
+//     for( let j=0; j<=i; j++){
+//         console.log(i,j)
+//     }
+// }
+
+// for(let i=0; i<3; i++){
+//     for( let j=i; j>=0; j--){
+//         console.log(i,j)
+//     }
+// }
+
+for(let i=5; i>0; i--){
+    for( let j=0; j < i; j++){
+        console.log(i,j)
+    }
+}
